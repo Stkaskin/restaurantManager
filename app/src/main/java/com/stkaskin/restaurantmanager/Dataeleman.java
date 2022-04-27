aa5da9cfaeacfcc3194d27edae1ad9ab1c1ee28f
Stkaskin <stkaskin01@gmail.com>
2022-04-28 01:50:26 +0300

    third commit

--- app/src/main/java/com/stkaskin/restaurantmanager/Dataeleman.java
+++ app/src/main/java/com/stkaskin/restaurantmanager/Dataeleman.java
@@ -1,15 +1,15 @@
-package modules;
+package com.stkaskin.restaurantmanager;
 
 import java.util.ArrayList;
 
-public class Data {
+public class Dataeleman {
     static  String garson ="Garson";
     static  String asci = "Aşçı";
     static  String mudur = "Müdür";
 
 
   static   public ArrayList<String> getArrayListElemanlar() {
-        ArrayList<String> arrayList = new ArrayList<String>();
+        ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add(garson);
         arrayList.add(asci);
         arrayList.add(mudur);
--- app/src/main/java/com/stkaskin/restaurantmanager/MainActivity.java
+++ app/src/main/java/com/stkaskin/restaurantmanager/MainActivity.java
@@ -3,6 +3,9 @@ package com.stkaskin.restaurantmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList liste=Dataeleman.getArrayListElemanlar();
+        TextView v=findViewById(R.id.textView);
+        v.setText("");
+        for (String item:Dataeleman.getArrayListElemanlar()) {
+            v.append(item);
+        }
+
     }
 }
